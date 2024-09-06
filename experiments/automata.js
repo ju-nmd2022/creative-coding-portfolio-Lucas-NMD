bb81f24a3ee72233ffe01ec0bd18738c6f7e8dac
LucasNMD <rolu22fh@student.ju.se>
2024-09-06 03:00:40 +0200

    a

--- experiments/automata.js
+++ experiments/automata.js
@@ -1,100 +1,124 @@
 function setup() {
-    createCanvas(800, 800);
+  createCanvas(800, 800);
+  frameRate(5);
+}
+
+class Cell {
+  constructor(x, y, state) {
+    this.x = x;
+    this.y = y;
+    this.state = state;
+    this.newState = -1;
+    this.color = this.getRandomColor();
   }
-  
-  class Cell {
-    constructor(x, y, state) {
-      this.x = x;
-      this.y = y;
-      this.state = state;
-      this.newState = -1;
-    }
-  
-    draw(size) {
-      let r = random(0, 255);
-      let g = random(0, 255);
-      let b = random(0, 255);
-      
-      if (this.state == 0) {
-        fill(255, 255, 255);
-      } else {
-        fill(r, g, b);
-      }
-      rect(this.x * size, this.y * size * 1.4, size, size * 2);
-    }
+
+  getRandomColor() {
+    const colors = [
+      color(255, 255, 255),
+      color(255, 0, 0),
+      color(0, 0, 255),
+      color(255, 255, 0),
+      color(0, 0, 0) 
+    ];
+    return colors[Math.floor(random(colors.length))];
   }
-  
-  let board = [];
-  let size = 3;
-  let lifecycle = 5;
-  let count = 0;
-  let boardsize = 200;
- 
-  
-  for (let i = 0; i < boardsize; i++) {
+
+  toggleState() {
+    this.state = 1 - this.state;
+    this.color = this.getRandomColor();
+  }
+
+  draw(size) {
+    fill(this.color);
+    rect(this.x * size, this.y * size, size, size);
+  }
+}
+
+let board = [];
+let size = 40; 
+let boardSize = 20;
+
+function setupGrid() {
+  for (let i = 0; i < boardSize; i++) {
     board.push([]);
-    for (let j = 0; j < boardsize; j++) {
-      let state = Math.round(Math.random() % 1);
+    for (let j = 0; j < boardSize; j++) {
+      let state = Math.round(Math.random());
       let cell = new Cell(i, j, state);
       board[i].push(cell);
     }
   }
-  
-  function calculateNewState(x, y) {
-    // count neighbour cells
-    let startX = Math.max(0, x - 1);
-    let startY = Math.max(0, y - 1);
-    let endX = Math.min(board.length, x +2);
-    let endY = Math.min(board[x].length, y * 1.3);
-  
-    let values = [];
-    for (let i = startX; i < endX; i++) {
-      let cells = board[i].slice(startY, endY);
-      values = values.concat(cells);
+}
+
+function drawMondrianGrid() {
+  stroke(0);
+  strokeWeight(8);
+  for (let i = 0; i <= boardSize; i++) {
+    line(i * size, 0, i * size, height);
+    line(0, i * size, width, i * size);
+  }
+}
+
+function draw() {
+  background(255);
+  noStroke();
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      board[i][j].draw(size);
     }
-    // filter for 1/0
-    let liveCells = values.filter((cell) => cell.state === 1);
-    let currentState = board[x][y].state;
-    if (liveCells.length === 3) {
-      board[x][y].newState = 1;
-    } else if (liveCells.length === 4) {
-      board[x][y].newState = currentState;
-    } else {
+  }
+
+  drawMondrianGrid();
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      calculateNewState(i, j);
+    }
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      board[i][j].state = board[i][j].newState;
+    }
+  }
+}
+
+function calculateNewState(x, y) {
+  let liveNeighbors = 0;
+
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (i === 0 && j === 0) continue;
+      let ni = (x + i + boardSize) % boardSize;
+      let nj = (y + j + boardSize) % boardSize;
+      liveNeighbors += board[ni][nj].state;
+    }
+  }
+
+  if (board[x][y].state === 1) { 
+    if (liveNeighbors < 2 || liveNeighbors > 3) {
       board[x][y].newState = 0;
+    } else {
+      board[x][y].newState = 1;
     }
-    // 2 = 0
-    // 4 = same
-    // 3 = 1
-    // >5 = 0
-  }
-  
-  function calculateLiving() {
-    let flat = board.flat();
-    let mapped = flat.map((cell) => cell.state);
-    let living = mapped.reduce((acc, cur) => acc + cur);
-    console.log(living);
-  }
-  
-  function draw() {
-    if (count == 0) {
-      noStroke();
-      for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-          board[i][j].draw(size);
-          calculateNewState(i, j);
-        }
-      }
-      calculateLiving();
-  
-      for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-          board[i][j].state = board[i][j].newState;
-        }
-      }
-    }
-    count++;
-    if (count == lifecycle) {
-      count = 0;
+  } else {  
+    if (liveNeighbors === 3) {
+      board[x][y].newState = 1;  
+      board[x][y].color = board[x][y].getRandomColor();  
+    } else {
+      board[x][y].newState = 0;  
     }
   }
-  
+}
+
+function mousePressed() {
+  let i = Math.floor(mouseX / size);
+  let j = Math.floor(mouseY / size);
+
+  if (i >= 0 && i < boardSize && j >= 0 && j < boardSize) {
+    board[i][j].toggleState();  
+    redraw(); 
+  }
+}
+
+setupGrid();
